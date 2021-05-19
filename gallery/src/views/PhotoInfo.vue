2afbe56d2db871355e0a2dc4c302b3d9dcbba468
Vlad Kovalev <vladsmit88@gmail.com>
2021-05-19 11:08:08 +0300

    corrected router condition

--- gallery/src/views/PhotoInfo.vue
+++ gallery/src/views/PhotoInfo.vue
@@ -91,7 +91,7 @@ export default {
         if (
             this.$route.params.category > 4 ||
             this.$route.params.id > this.$route.params.category * 6 ||
-            this.$route.params.id < (this.$route.params.category - 1) * 6 ||
+            this.$route.params.id <= (this.$route.params.category - 1) * 6 ||
             /\D/.test(this.$route.params.category) ||
             /\D/.test(this.$route.params.id)
         ) {
